d580be1953a5ea8ab5ebebb152e1804fe96e56bc
4ngelpilin <angelhernandez13985@gmail.com>
2024-05-25 18:59:21 -0600

    ft Agregado lista de productos

--- index.php
+++ index.php
@@ -9,14 +9,12 @@
     <?php
         include_once './fuente/conexion_bd.php';
         $query = "select nombre from clientes;";
-        $consulta = mysqli_query($conexion,$query);
-        /*
-        while($fila = mysqli_fetch_assoc($consulta)){
-            echo $fila["nombre"];
-        }
-        */
+        $consulta_clientes = mysqli_query($conexion,$query);
+        $query = "select p.nombre as producto ,p.precio as precio,c.nombre as categoria from productos p inner join categorias c on c.id = p.categoria_id;";
+        $consulta_productos = mysqli_query($conexion,$query);
     ?>
     <h1>Bienvenido a su Tiendita de tecnologia Paquito</h1>
+    <img src="recursos/hola.jpg" alt="">
     <h3>CLIENTES CONTENTOS</h3>
     <table>
         <tr>
@@ -24,14 +22,33 @@
         </tr>
         <?php
           
-          while($fila = mysqli_fetch_assoc($consulta)){
+          while($fila = mysqli_fetch_assoc($consulta_clientes)){
             echo "<tr>";
             echo "<td>".$fila["nombre"]."</td>";
             echo "</tr>";
         }
          
         ?>
-    </table>
+</table>
+<h3>Productos</h3>
+<table>
+    <tr>
+        <th>nombre</th>
+        <th>precio</th>
+        <th>categotia</th>
+    </tr>
+    <?php
+          
+          while($fila = mysqli_fetch_assoc($consulta_productos)){
+            echo "<tr>";
+            echo "<td>".$fila["producto"]."</td>";
+            echo "<td>$".$fila["precio"]."</td>";
+            echo "<td>".$fila["categoria"]."</td>";
+            echo "</tr>";
+        }
+         
+        ?>echo "<td>".$fila["nombre"]."</td>";
+</table>
     
 </body>
 </html>
